49a2b4f01e8b740f19fdf44cbf6f0bfbcebe5fd4
Cloud User <centos@ip-172-31-14-81.us-west-2.compute.internal>
2017-03-16 03:48:14 +0000

    got a google map on it, need to make a way for truck to move

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="http://thrashcan.net/includes/jquery-3.1.1.min.js"></script>
+  <link rel="stylesheet" href="http://thrashcan.net/includes/bootstrap.min.css">
+  <script src="http://thrashcan.net/includes/bootstrap.min.js"></script>
+  <link rel="stylesheet"
+    href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="main.css?v=a">
+</head>
+<body>
+  <div class="container">
+    <div class="row" id="intro">
+      <div class="col-lg-4 col-xs-12 text-center">
+        <img src="pics/bus.gif" />
+      </div>
+      <div class="col-lg-6 col-xs-12 text-center">
+        <h2>Welcome to The Cheesesteak Academy!</h2>
+      </div>
+    </div>
+
+    <div class="row text-center" id="comingSoon">
+      <h2>Coming Soon!!</h2>
+    </div>
+
+    <div class="row text-center" id="aboutChris">
+      <div class="col-lg-12 col-xs-12">
+        <h3>About the Owner:</h3>
+        <p>In publishing and graphic design, lorem ipsum (derived from Latin
+          dolorem ipsum, translated as "pain itself") is a filler text commonly
+          used to demonstrate the graphic elements of a document or visual presentation.
+          Replacing meaningful content with placeholder text allows designers to
+          design the form of the content before the content itself has been produced.[citation needed]
+          The lorem ipsum text is typically a scrambled section of De finibus bonorum
+          et malorum, a 1st-century BC Latin text by Cicero, with words altered,
+          added, and removed to make it nonsensical, improper Latin.[citation needed]
+          A variation of the ordinary lorem ipsum text has been used in typesetting since
+          the 1960s or earlier, when it was popularized by advertisements for Letraset
+          transfer sheets. It was introduced to the Information Age in the mid-1980s
+          by Aldus Corporation, which employed it in graphics and word-processing
+          templates for its desktop publishing program PageMaker.
+        </p>
+      </div>
+    </div>
+
+    <div class="row" id="truckFinder">
+      <div class="col-lg-6 col-xs-12">
+        <h4>Don't miss the bus!</h4>
+        <iframe
+          width="100%"
+          height="350px"
+          frameborder="0" style="border:0"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAb_y9cQ9NdEHYasf3R2UnTLS4kMRe_W9M&q=1706+Heil+Quaker+Blvd+La+Vergne+TN" allowfullscreen>
+        </iframe>
+      </div>
+      <div class="col-lg-6 col-xs-12">
+        <img src="/pics/cheese-steak-629342_640.jpg">
+      </div>
+    </div>
+  </div>
+  </div>
+</body>
